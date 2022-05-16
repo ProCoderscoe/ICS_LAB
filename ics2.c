009228ea1d5d79c742620bbacf03d531366b92ba
ProCoderscoe <105592614+ProCoderscoe@users.noreply.github.com>
2022-05-16 20:20:18 +0530

    Update ics2.c

--- ics2.c
+++ ics2.c
@@ -1,68 +1,60 @@
-// C program for RSA asymmetric cryptographic 
-// algorithm. For demonstration values are 
-// relatively small compared to practical 
-// application 
-#include<stdio.h> 
-#include<math.h> 
+#include<stdio.h>
+#include<math.h>
+//to find gcd
+int gcd(int a, int h)
+{
+	int temp;
+	while(1)
+	{
+temp = a%h;
+if(temp==0)
+return h;
+a = h;
+h = temp;
+}
+}
 
-// Returns gcd of a and b 
-int gcd(int a, int h) 
-{ 
-	int temp; 
-	while (1) 
-	{ 
-		temp = a%h; 
-		if (temp == 0) 
-		return h; 
-		a = h; 
-		h = temp; 
-	} 
-} 
-
-// Code to demonstrate RSA algorithm 
-int main() 
-{ 
-	// Two random prime numbers 
-	double p = 3; 
-	double q = 7; 
-
-	// First part of public key: 
-	double n = p*q; 
-
-	// Finding other part of public key. 
-	// e stands for encrypt 
-	double e = 2; 
-	double phi = (p-1)*(q-1); 
-	while (e < phi) 
-	{ 
-		// e must be co-prime to phi and 
-		// smaller than phi. 
-		if (gcd(e, phi)==1) 
-			break; 
-		else
-			e++; 
-	} 
-
-	// Private key (d stands for decrypt) 
-	// choosing d such that it satisfies 
-	// d*e = 1 + k * totient 
-	int k = 2; // A constant value 
-	double d = (1 + (k*phi))/e; 
-
-	// Message to be encrypted 
-	double msg = 20; 
-
-	printf("Message data = %lf", msg); 
-
-	// Encryption c = (msg ^ e) % n 
-	double c = pow(msg, e); 
-	c = fmod(c, n); 
-	printf("\nEncrypted data = %lf", c); 
-
-	// Decryption m = (c ^ d) % n 
-	double m = pow(c, d); 
-	m = fmod(m, n); 
-	printf("\nOriginal Message Sent = %lf", m); 
-
-	return 0; 
-} 
+int main()
+{
+//2 random prime numbers
+double p = 3;
+double q = 7;
+double n=p*q;
+double count;
+double totient = (p-1)*(q-1);
+//public key
+//e stands for encrypt
+double e=2;
+//for checking co-prime which satisfies e>1
+while(e<totient)
+{
+count = gcd(e,totient);
+if(count==1)
+break;
+else
+e++;
+}
+//private key
+//d stands for decrypt
+double d;
+//k can be any arbitrary value
+double k = 2;
+//choosing d such that it satisfies d*e = 1 + k * totient
+d = (1 + (k*totient))/e;
+double msg = 12;
+double c = pow(msg,e);
+double m = pow(c,d);
+c=fmod(c,n);
+m=fmod(m,n);
+printf("Message data = %lf",msg);
+printf("\np = %lf",p);
+printf("\nq = %lf",q);
+printf("\nn = pq = %lf",n);
+printf("\ntotient = %lf",totient);
+printf("\ne = %lf",e);
+printf("\nd = %lf",d);
+printf("\nEncrypted data = %lf",c);
+printf("\nOriginal Message Sent = %lf",m);
+return 0;
+    
+}
